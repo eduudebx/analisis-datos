53c66d5702899c17ae6288d837875af73ca66c34
eduu-est <mendieta.c.est@gmail.com>
2024-08-13 21:43:35 -0500

    Agregando tarea de programación sobre listas.

--- ciclo-1/programacion/tareas/tarea-3.py
+++ ciclo-1/programacion/tareas/tarea-3.py
@@ -1,11 +1,16 @@
-numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14]
-numeros_2 = [15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28]
+lista_1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14]
+lista_2 = lista_1[::-1]
 
-pares = [i for i in numeros if i % 2 == 0]
+print(f'\nLista 1: {lista_1} | Lista 2: {lista_2}')
 
-impares = [i for i in numeros if i % 2 != 0]
+pares, impares, multiplicaciones = [], [], []
 
-multiplicaciones = [numeros[i] * numeros_2[i] for i in range(len(numeros))]
+for i in range(len(lista_1)):
+    if lista_1[i] % 2 == 0:
+        pares.append(lista_1[i])
+    else:
+        impares.append(lista_1[i])
+    multiplicaciones.append(lista_1[i] * lista_2[i])
 
-print(f'\nLista1: {numeros}\nLista2: {numeros_2}\n\nPares: {pares}\nImpares: {impares}\nMultiplicaciones: {multiplicaciones}\n')
+print(f'\nPares: {pares}\nImpares: {impares}\nMultiplicaciones: {multiplicaciones}\n')
 
