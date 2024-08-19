ca91594597fbc8bc724093758456600d625994f9
eduu-est <mendieta.c.est@gmail.com>
2024-08-19 17:52:29 -0500

    Corrigiendo Tarea 3 de visualización.

--- ciclo-1/intro-ad/tareas/tarea-3/visualizacion/main.R
+++ ciclo-1/intro-ad/tareas/tarea-3/visualizacion/main.R
@@ -2,69 +2,30 @@ library(readxl)
 library(dplyr)
 library(stringr)
 library(openxlsx)
+library(RColorBrewer)
+library(plotrix)
 
-limpieza <- function(){
-        # LIMPIEZA: -----------------------------------------------------------------------------
+
+limpiar <- function(){
+       
     datos <- read_excel('ds-ventallantas-original.xlsx')
     datos_limpios <- datos[!apply(datos, 1, function(x) all(is.na(x) | x == "")), ]
 
     datos_limpios <- datos_limpios %>% arrange(Empleado)
-
-    #datos_repetidos <- datos_limpios[duplicated(datos_limpios) | duplicated(datos_limpios, fromLast = TRUE), ]
-    #print(paste('Datos repetidos:', nrow(datos_repetidos)))
-    # Datos repetidos: 24923.
-
-    #View(datos_repetidos)
-
     datos_limpios <- datos_limpios[!duplicated(datos_limpios) & !duplicated(datos_limpios, fromLast = TRUE), ]
-    #print(paste('El dataset ahora tiene:', nrow(datos_limpios), ', luego de eliminar las filas repetidas.'))
-    # El dataset ahora tiene: 102332 , luego de eliminar las filas repetidas.
 
-    #print(colSums(is.na(datos_limpios)))
-
-
-    # VALIDACIÓN DE DATOS ----------------------------------------------------------------------
-
-    # 1. VALIDANDO COLUMNAS NUMERICAS
-    datos_limpios$Empleado <- as.numeric(datos_limpios$Empleado)
-    #print(sum(is.na(datos_limpios$Empleado) | datos_limpios$Empleado <= 0 | datos_limpios$Empleado != floor(datos_limpios$Empleado)))
-    # Respuesta: 0
-
+    datos_limpios$Empleado <- as.numeric(datos_limpios$Empleado) 
     datos_limpios$CodigoFamilia <- as.numeric(datos_limpios$CodigoFamilia)
-    #print(sum(is.na(datos_limpios$CodigoFamilia) | datos_limpios$CodigoFamilia <= 0 | datos_limpios$CodigoFamilia != floor(datos_limpios$CodigoFamilia)))
-    # Respuesta: 0
-
     datos_limpios$Ventas <- as.numeric(datos_limpios$Ventas)
-    #print(sum(is.na(datos_limpios$Ventas) | datos_limpios$Ventas <= 0 | datos_limpios$Ventas != floor(datos_limpios$Ventas)))
-    # Respuesta: 0
-
     datos_limpios$Area <- as.numeric(datos_limpios$Area)
-    #print(sum(is.na(datos_limpios$Area) | datos_limpios$Area <= 0 | datos_limpios$Area != floor(datos_limpios$Area)))
-    # Respuesta: 0
-
     datos_limpios$Cantidad <- as.numeric(datos_limpios$Cantidad)
-    #print(sum(is.na(datos_limpios$Cantidad)))# Respuesta: 0
-    #print(sum(datos_limpios$Cantidad <= 0)) # Respuesta: 0
-    #print(sum(datos_limpios$Cantidad != floor(datos_limpios$Cantidad))) # Respuesta: 2
-
-    #valores_no_enteros <- datos_limpios$Cantidad[datos_limpios$Cantidad != floor(datos_limpios$Cantidad)]
-    #print(valores_no_enteros) #  1.068 1.006
 
     datos_limpios$Cantidad <- ifelse(datos_limpios$Cantidad != floor(datos_limpios$Cantidad), 
                                     ceiling(datos_limpios$Cantidad), 
                                     datos_limpios$Cantidad)
 
-
-    # VALIDACION DE FECHAS:
-    #datos_limpios$Fecha <- as.character(datos_limpios$Fecha)
-    #print(sum(!is.na(datos_limpios$Fecha) & !is.na(as.Date(datos_limpios$Fecha, format = "%m/%d/%Y"))))
-    #Respuesta: 0
-
     datos_limpios$Fecha <- format(as.Date(datos_limpios$Fecha, format = "%m/%d/%Y"), "%d/%m/%Y")
-    #View(datos_limpios)
 
-
-    # CORRIGIENDO FORMATOS STRING:
 
     datos_limpios <- datos_limpios %>%
         mutate(
@@ -75,73 +36,72 @@ limpieza <- function(){
             Sede = str_to_title(Sede)
         )
 
-    #View(datos_limpios)
-
-
     write.xlsx(datos_limpios, 'datos-limpios.xlsx')
 }
 
 
+grafica1 <- function(datos_limpios){
+
+    par(mfrow = c(2, 3))
+
+    anios <- unique(datos_limpios$AnioVenta)
+
+    for(anio in anios){
+        datos_filtrados <- datos_limpios[datos_limpios$AnioVenta == anio, ]
+        nombres_unicos <- sort(unique(datos_filtrados$NombreEmpleado))
+
+        ventas_empleados <- vector('numeric')
+        for (nombre in nombres_unicos) {
+            filtro <- datos_limpios[datos_limpios$NombreEmpleado == nombre, ]
+            ventas_empleados <- c(ventas_empleados, length(filtro$Ventas))
+        }
+
+        barplot(height = ventas_empleados, names = nombres_unicos, horiz = 1,  las = 1,
+                main = paste('Total de ventas por empleado en el año', anio))
+        grid(col = '#2B434F')
+    }
+}
+
+
+grafica2 <- function(datos_limpios){
+
+    categorias <- unique(datos_limpios$Familia)
+    
+    cantidades <- vector('numeric')
+    for(categoria in categorias){
+        datos_filtrados <- datos_limpios[datos_limpios$Familia == categoria, ]
+        cantidades <- c(cantidades, length(datos_filtrados$Familia))
+    }
+    porcentajes <- round(cantidades / sum(cantidades) * 100, 1)
+    pie3D(cantidades, labels = paste(categorias, porcentajes, '%', sep=' '), explode = 0.1, 
+          main = 'Porcentaje de categorias vendidas')
+}
+
+
+grafica3 <- function(datos_limpios){
+    sedes = unique(datos_limpios$Sede)
+    anios = unique(datos_limpios$AnioVenta)
+
+    
+}
+
+
 main <- function(){
+
+    limpiar()
+
     datos_limpios <- read_excel('datos-limpios.xlsx')
-    #print(nrow(datos))
-
-    #par(mfrow = c(1, 2))
-
-    # GRÁFICA 1: ---------------------------------------------------
-    frecuencias_familia <- table(datos_limpios$Familia)
-    empleado_ventas <- table(datos_limpios$Empleado)
-
-    #barplot(frecuencias_familia, 
-     #   col = '#33B8FF', 
-      #  xlab = 'Categoría', 
-       # ylab = 'Cantidad de ventas', 
-      #  main = 'Ventas por categoría de producto', 
-      #  las = 1)
-    #grid(nx = NA, ny = NULL, lty = 2, col = '#1B060B', lwd = 2)
-
-    # GRÁFICA 2: ---------------------------------------------------
 
     datos_limpios$Fecha <- as.Date(datos_limpios$Fecha, format = "%d/%m/%Y")
     datos_limpios$AnioVenta <- format(datos_limpios$Fecha, "%Y")
+    datos_limpios$NombreEmpleado <- paste("Emp", datos_limpios$Empleado)
+
     datos_limpios$AnioVenta <- as.numeric(as.character(datos_limpios$AnioVenta))
 
-    datos_limpios$NombreEmpleado <- paste("Empleado", datos_limpios$Empleado)
-
-
-    ultimo_anio <- max(datos_limpios$AnioVenta)
-
-    # Filtrar los empleados que tienen ventas mayores a cero en el último año
-    empleados_filtrados <- unique(datos_limpios$NombreEmpleado[
-    datos_limpios$AnioVenta == ultimo_anio & datos_limpios$Ventas > 0
-    ])
-
-    # Filtrar los datos para incluir solo los empleados seleccionados
-    datos_filtrados <- subset(datos_limpios, NombreEmpleado %in% empleados_filtrados)
-
-    ventas_por_empleado_anio <- datos_filtrados %>%
-  group_by(NombreEmpleado, AnioVenta) %>%
-  summarise(Ventas = sum(Ventas), .groups = 'drop') %>%
-  spread(key = AnioVenta, value = Ventas, fill = 0)
-
-  # Preparar el gráfico
-plot(ventas_por_empleado_anio[,2], type = "l", col = 1, lwd = 2,
-     xlab = "Año", ylab = "Número de Ventas",
-     main = "Número de Ventas por Empleado en Diferentes Años",
-     xaxt = "n")
-
-# Añadir las líneas para los otros empleados
-for (i in 2:ncol(ventas_por_empleado_anio)) {
-  lines(ventas_por_empleado_anio[,i], col = i)
+    grafica1(datos_limpios)
+    grafica2(datos_limpios)
+    grafica3(datos_limpios)
 }
 
-# Añadir las etiquetas de los ejes x
-axis(1, at = 1:ncol(ventas_por_empleado_anio), labels = colnames(ventas_por_empleado_anio)[-1])
-
-# Añadir leyenda
-legend("topright", legend = ventas_por_empleado_anio$NombreEmpleado, col = 1:nrow(ventas_por_empleado_anio), lty = 1, lwd = 2, title = "Empleados")
-
-
-}
 
 main()
