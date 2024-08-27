21eb3d07ab86118812d45bb817e0d701a600f5e9
eduu-est <mendieta.c.est@gmail.com>
2024-08-26 20:27:58 -0500

    Agregando tarea con interfaz gráfica en Python.

---
+++ ciclo-1/programacion/tareas/tarea9/tarea9.py
@@ -0,0 +1,87 @@
+import tkinter as tk
+from tkinter import ttk
+from PIL import Image, ImageTk
+
+
+# Ventana principal: ------------------------------------------------------------------------
+ventana = tk.Tk()
+ventana.title('Registro de mascotas')
+ventana.geometry('800x300')
+
+
+# Etiquetas: --------------------------------------------------------------------------------
+lbl_id = tk.Label(ventana, text='id:', padx=5, pady=10)
+lbl_foto = tk.Label(ventana, text='Foto:', padx=5, pady=10)
+lbl_nombre = tk.Label(ventana, text='Nombre:', padx=5, pady=10)
+lbl_apellido = tk.Label(ventana, text='Apellido:', padx=5, pady=10)
+lbl_estatura = tk.Label(ventana, text='Estatura:', padx=5, pady=10)
+lbl_peso = tk.Label(ventana, text='Peso:', padx=5, pady=10)
+lbl_fecha_nac = tk.Label(ventana, text='Fecha de Nacimiento:', padx=5, pady=10)
+lbl_raza = tk.Label(ventana, text='Raza:', padx=5, pady=10)
+lbl_sexo = tk.Label(ventana, text='Sexo:', padx=5, pady=10)
+
+lbl_id.grid(column = 0, row = 0)
+lbl_foto.grid(column = 2, row = 0)
+lbl_nombre.grid(column = 0, row = 1)
+lbl_apellido.grid(column = 2, row = 1)
+lbl_estatura.grid(column = 0, row = 2)
+lbl_peso.grid(column = 2, row = 2)
+lbl_fecha_nac.grid(column = 0, row = 3)
+lbl_raza.grid(column = 2, row = 3)
+lbl_sexo.grid(column = 0, row = 4)
+
+
+# Selector de imagen: -----------------------------------------------------------------------
+foto = tk.PhotoImage(file='perro.png')
+lbl_foto_mascota = ttk.Label(ventana, image=foto)
+lbl_foto_mascota.grid(column = 3, row = 0)
+
+
+# Cajas de texto: ---------------------------------------------------------------------------
+txt_id = ttk.Entry(ventana)
+txt_nombre = ttk.Entry(ventana)
+txt_apellido = ttk.Entry(ventana)
+txt_estatura = ttk.Entry(ventana)
+txt_peso = ttk.Entry(ventana)
+txt_fecha_nac = ttk.Entry(ventana)
+
+txt_id.grid(column=1, row=0)
+txt_nombre.grid(column=1, row=1)
+txt_apellido.grid(column=3, row=1)
+txt_estatura.grid(column=1, row=2)
+txt_peso.grid(column=3, row=2)
+txt_fecha_nac.grid(column=1, row=3)
+
+
+# Combobox: ---------------------------------------------------------------------------------
+combo_raza = ttk.Combobox(ventana, 
+                          values=['Mestizo', 'Labrador Retriever', 'Pastor Alemán', 
+                                  'Bulldog Inglés', 'Poodle', 'Beagle', 'Golden Retriever', 
+                                  'Rottweiler', 'Chihuahua', 'Dachshund', 'Otro'])
+combo_raza.set('Mestizo')
+
+combo_raza.grid(column=3, row=3)
+
+
+# Botones de radio: ----------------------------------------------------------------------------
+rbtn_sexo_selec = tk.IntVar(value=1)
+rbtn_hembra = tk.Radiobutton(ventana, text='Hembra', variable=rbtn_sexo_selec, value=1)
+rbtn_macho = tk.Radiobutton(ventana, text='Macho', variable=rbtn_sexo_selec, value=2)
+
+rbtn_hembra.grid(column=1, row=4)
+rbtn_macho.grid(column=2, row=4)
+
+
+# Botones: ----------------------------------------------------------------------------------
+def registrar_mascota():
+    pass
+
+btn_registrar = tk.Button(ventana, text='Registrar', command=registrar_mascota, pady=5)
+btn_salir = tk.Button(ventana, text='Salir', command=ventana.destroy, pady=5)
+
+btn_registrar.grid(column=1, row=5)
+btn_salir.grid(column=2, row=5)
+
+
+# Ejecución de la ventana: ------------------------------------------------------------------
+ventana.mainloop()
