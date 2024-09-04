910f8db4917c1891c0fd830734537ccfd2232888
eduu-est <mendieta.c.est@gmail.com>
2024-09-03 22:31:34 -0500

    .

--- ciclo-1/estadistica/proyecto-final/main.R
+++ ciclo-1/estadistica/proyecto-final/main.R
@@ -1,16 +1,17 @@
 library(readxl)
 
 
-contar_coincidencias <- function(set_datos, nombre_columna){
+# OPERACIONES -------------------------------------------------------------------------------
+obtener_coincidencias <- function(set_datos, nombre_columna){
     columna <- set_datos[[nombre_columna]]
     conteo <- as.data.frame(table(columna))
     etiquetas <- as.vector(conteo[, 1])
-    ocurrencias <- as.vector(conteo[, 2])
-    return(list(etiquetas = etiquetas, ocurrencias = ocurrencias))
+    coincidencias <- as.vector(conteo[, 2])
+    return(list(etiquetas = etiquetas, num_coincidencias = coincidencias))
 }
 
 
-gen_tbl_frecuencias_acum <- function(set_datos, nombre_columna, etiqueta_columna){
+gen_tbl_frecuencias_acum <- function(set_datos, nombre_columna, etiqueta_columna, ver_tabla){
     datos <- as.vector(set_datos[[nombre_columna]])
     
     num_datos <- length(datos)
@@ -19,33 +20,53 @@ gen_tbl_frecuencias_acum <- function(set_datos, nombre_columna, etiqueta_columna
     clases <- nclass.Sturges(datos)
     amplitud <- ceiling((maximo - minimo) / clases)
 
-    print(paste('n =', num_datos, '| Min =', minimo, '| Max =', maximo))
-    print(paste('k =', clases, '| a =', amplitud))
-
     extremos <- minimo + amplitud * (0: clases)
-    extremo_inferior <- minimo + amplitud * (0: (clases - 1))
-    extremo_superior <- minimo + amplitud * (1: clases)
-    marca_clase <- (extremo_inferior + extremo_superior) / 2
     
-    factor <- cut(datos, breaks = extremos, right = FALSE, include.lowest = TRUE)
-    intervalos <- levels(factor)
+    if(ver_tabla){
+        extremo_inferior <- minimo + amplitud * (0: (clases - 1))
+        extremo_superior <- minimo + amplitud * (1: clases)
+        marca_clase <- (extremo_inferior + extremo_superior) / 2
+        
+        factor <- cut(datos, breaks = extremos, right = FALSE, include.lowest = TRUE)
+        intervalos <- levels(factor)
 
-    frecuencia_absoluta <- as.vector(table(factor))
-    frecuencia_relativa <- round(frecuencia_absoluta / num_datos, digits = 2)
-    frecuencia_absoluta_acum <- cumsum(frecuencia_absoluta)
-    frecuencia_relativa_acum <- cumsum(frecuencia_relativa)
-    frecuencia_porcentual <- paste(round(frecuencia_relativa * 100, digits = 2), '%')
+        frecuencia_absoluta <- as.vector(table(factor))
+        frecuencia_relativa <- round(frecuencia_absoluta / num_datos, digits = 2)
+        frecuencia_absoluta_acum <- cumsum(frecuencia_absoluta)
+        frecuencia_relativa_acum <- cumsum(frecuencia_relativa)
+        frecuencia_porcentual <- paste(round(frecuencia_relativa * 100, digits = 2), '%')
 
-    tabla <- data.frame(intervalos, marca_clase, frecuencia_absoluta, frecuencia_absoluta_acum, 
-                        frecuencia_relativa, frecuencia_relativa_acum, frecuencia_porcentual)
+        print(paste('n =', num_datos, '| Min =', minimo, '| Max =', maximo))
+        print(paste('k =', clases, '| a =', amplitud))
 
-    colnames(tabla) <- c(etiqueta_columna, 'Marca de Clase', 'Frecuencia Absoluta', 'Frecuencia Absoluta Acumulada', 
-                         'Frecuencia Relativa', 'Frecuencia Relativa Acumulada', 'Frecuencia Porcentual')
+        tabla <- data.frame(intervalos, marca_clase, frecuencia_absoluta, frecuencia_absoluta_acum, 
+                            frecuencia_relativa, frecuencia_relativa_acum, frecuencia_porcentual)
 
+        colnames(tabla) <- c(etiqueta_columna, 'Marca de Clase', 'Frecuencia Absoluta', 'Frecuencia Absoluta Acumulada', 
+                            'Frecuencia Relativa', 'Frecuencia Relativa Acumulada', 'Frecuencia Porcentual')
+
+        View(tabla)
+    }
+
+    return(list(datos = datos, extremos = extremos))
+}
+
+
+gen_tbl_frecuencias <- function(set_datos, nombre_columna, etiqueta_columna){
+    datos_frecuecias <- obtener_coincidencias(set_datos, nombre_columna)
+    tabla <- data.frame(datos_frecuecias$etiquetas, datos_frecuecias$num_coincidencias)
+    colnames(tabla) <- c(etiqueta_columna, 'Frecuencias')
     View(tabla)
 }
 
 
+# VISUALIZACIÓN -----------------------------------------------------------------------------
+gen_histograma <- function(){
+    
+}
+
+
+# PRINCIPAL ---------------------------------------------------------------------------------
 main <- function(){
     set_datos <- read_excel('datos.xlsx')
 
@@ -53,6 +74,8 @@ main <- function(){
     #gen_tbl_frecuencias_acum(set_datos, 'Horas_Suenio_Sem', 'Horas de sueño')
     #gen_tbl_frecuencias_acum(set_datos, 'Horas_Estudio_Sem', 'Horas de estudio')
     #gen_tbl_frecuencias_acum(set_datos, 'Horas_Diarias_Redes', 'Horas en redes')
+
+    #gen_tbl_frecuencias(set_datos, 'Nivel_Previo', 'Nivel de instrucción previa')
 }
 
 
