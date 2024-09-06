38795f342b201909f01fe68d1d47ae8f4bbb8ce2
eduu-est <mendieta.c.est@gmail.com>
2024-09-06 17:33:11 -0500

    .

--- ciclo-1/estadistica/proyecto-final/main.R
+++ ciclo-1/estadistica/proyecto-final/main.R
@@ -22,15 +22,17 @@ gen_tbl_frecuencias_acum <- function(set_datos, nombre_columna, etiqueta_columna
 
     extremos <- minimo + amplitud * (0: clases)
     
-    if(ver_tabla){
-        extremo_inferior <- minimo + amplitud * (0: (clases - 1))
-        extremo_superior <- minimo + amplitud * (1: clases)
-        marca_clase <- (extremo_inferior + extremo_superior) / 2
+    
+    extremo_inferior <- minimo + amplitud * (0: (clases - 1))
+    extremo_superior <- minimo + amplitud * (1: clases)
+    marca_clase <- (extremo_inferior + extremo_superior) / 2
         
-        factor <- cut(datos, breaks = extremos, right = FALSE, include.lowest = TRUE)
-        intervalos <- levels(factor)
+    factor <- cut(datos, breaks = extremos, right = FALSE, include.lowest = TRUE)
+    intervalos <- levels(factor)
 
-        frecuencia_absoluta <- as.vector(table(factor))
+    frecuencia_absoluta <- as.vector(table(factor))
+        
+    if(ver_tabla){
         frecuencia_relativa <- round(frecuencia_absoluta / num_datos, digits = 2)
         frecuencia_absoluta_acum <- cumsum(frecuencia_absoluta)
         frecuencia_relativa_acum <- cumsum(frecuencia_relativa)
@@ -48,7 +50,7 @@ gen_tbl_frecuencias_acum <- function(set_datos, nombre_columna, etiqueta_columna
         View(tabla)
     }
 
-    return(list(datos = datos, extremos = extremos))
+    return(list(datos = datos, extremos = extremos, frec_abs = frecuencia_absoluta))
 }
 
 
@@ -61,8 +63,19 @@ gen_tbl_frecuencias <- function(set_datos, nombre_columna, etiqueta_columna){
 
 
 # VISUALIZACIÓN -----------------------------------------------------------------------------
-gen_histograma <- function(){
+gen_histograma <- function(set_datos, nombre_columna, titulo, xlab, color){
+    datos <- gen_tbl_frecuencias_acum(set_datos, nombre_columna, '', ver_tabla = FALSE)
     
+    hist(datos$datos, breaks = datos$extremos, main = titulo, xlab = xlab, ylab = 'Frecuencias', right = F, axes = FALSE)
+    axis(1, datos$extremos)
+    axis(2, c(0, datos$frec_abs))
+    for(i in datos$frec_abs){
+        abline(h = i, col = '#466B7E', lty = 2)
+    }
+     for(i in datos$extremos){
+        abline(v = i, col = '#466B7E', lty = 2)
+    }
+    hist(datos$datos, add = TRUE, breaks = datos$extremos, right = F, axes = FALSE, col = color)
 }
 
 
@@ -76,6 +89,14 @@ main <- function(){
     #gen_tbl_frecuencias_acum(set_datos, 'Horas_Diarias_Redes', 'Horas en redes')
 
     #gen_tbl_frecuencias(set_datos, 'Nivel_Previo', 'Nivel de instrucción previa')
+
+    #gen_histograma(set_datos, 'Edad', 'Edades de los estudiantes de las carreras tecnológicas', 'Edades', '#D2B48C')
+    #gen_histograma(set_datos, 'Horas_Suenio_Sem', 'Horas de sueño a la semana', 'Horas de sueño', '#87cefa')
+    #gen_histograma(set_datos, 'Horas_Estudio_Sem', 'Horas de estudio a la semana', 'Horas de estudio', '#e59866')
+    #gen_histograma(set_datos, 'Horas_Diarias_Redes', 'Horas diarias en redes sociales', 'Horas en redes', '#80bbdb')
+
+
+    gen_tbl_frecuencias(set_datos, 'Sugerencias_Antes_Inicio_Carrera', 'Sugerencias antes del inicio de carrera')
 }
 
 
